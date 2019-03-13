aa392a859b0038f02ef4ab5d2b9012715786462d
ehhhhhha <ehhhhhha@163.com>
2019-03-13 15:52:29 +1100

    migration backup

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+	stock_sysmbol = models.CharField(max_length=10)
+	close_price = models.DecimalField(max_digits=7, decimal_places=2)
+	# to do: update() to fill later ?
+	date = models.DateField()
+	def __str__(self):
+		return self.stock_sysmbol
+		
--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import Stock
 
 def index(request):
-	return HttpResponse("Hello, this is the main page.")
+	latest_question_list = Stock.objects.order_by('date')[:5]
+	template = loader.get_template('main/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
