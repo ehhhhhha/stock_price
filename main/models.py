5fa362e897d8126cf547bef5973a57c61a37b146
ehhhhhha <ehhhhhha@163.com>
2019-03-13 16:47:34 +1100

    data loaded

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Stock(models.Model):
-	stock_sysmbol = models.CharField(max_length=10)
+	stock_symbol = models.CharField(max_length=10)
 	close_price = models.DecimalField(max_digits=7, decimal_places=2)
 	# to do: update() to fill later ?
 	date = models.DateField()
 	def __str__(self):
-		return self.stock_sysmbol
+		return self.stock_symbol
 		
--- main/views.py
+++ main/views.py
@@ -5,19 +5,19 @@ from django.template import loader
 from .models import Stock
 
 def index(request):
-	latest_question_list = Stock.objects.order_by('date')[:5]
+	latest_question_list = Stock.objects.order_by('date')[:10]
 	template = loader.get_template('main/index.html')
 	context = {
 		'latest_question_list': latest_question_list,
 	}
 	return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+# def detail(request, question_id):
+# 	return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+# def results(request, question_id):
+# 	response = "You're looking at the results of question %s."
+# 	return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+# def vote(request, question_id):
+# 	return HttpResponse("You're voting on question %s." % question_id)
